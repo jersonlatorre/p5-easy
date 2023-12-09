f4e649fcd4ad57e95e27eb66848a6c38144fe9ba
Jerson La Torre <jerson.latorre@gmail.com>
2023-12-09 00:22:55 -0500

    add: p.range implementation

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 import p5 from 'p5'
-export default new p5(() => {})
+let p = new p5(() => {})
+
+/* Range */
+p.range = (a, b = null, step = 1) => {
+  let start = b === null ? 0 : a
+  let end = b === null ? a : b
+  let range = []
+
+  for (let i = start; i < end; i += step) {
+    range.push(i)
+  }
+
+  return range
+}
+
+export default p
